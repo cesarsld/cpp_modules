d87b550e8074f23330fb277879c1dd308b4f2a31
cesarsld <jaimes.cesar88@gmail.com>
2020-03-25 18:39:34 +0100

    day01ex04

---
+++ day01/ex04/ex04.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex04.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjaimes <cjaimes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/25 18:35:27 by cjaimes           #+#    #+#             */
+/*   Updated: 2020/03/25 18:39:14 by cjaimes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *ptr = &str;
+	std::string	&ref = str;
+
+	std::cout << "Using pointer:\n" << std::endl;
+	std::cout << *ptr << std::endl;
+	std::cout << "Using reference:\n" << std::endl;
+	std::cout << ref << std::endl;
+	return (0);
+}
